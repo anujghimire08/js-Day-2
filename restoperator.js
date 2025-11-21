b7758eaf692bb528f8fe6194d73a9a549dbc0f39
Anuj Ghimire <anujghimire08@gmail.com>
2025-11-21 15:19:09 +0545

    remove unnecessary blank lines in rest operator example

--- restoperator.js
+++ restoperator.js
@@ -6,7 +6,6 @@ const user = {
 };
 
 const { name, caste, ...rest } = user;
-
-
+rest;
 //The rest operator (...) in JavaScript is used to collect multiple elements into a single array or object. It's basically the opposite of the spread operator.
 //It collects remaining arguments into an array.
